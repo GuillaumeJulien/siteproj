ff7f1d861eefecef01ce10b4fde075276bffca0a
Guillaume <Guillaume.Julien@etu.univ-savoie.fr>
2016-03-21 15:18:50 +0100

    animation blink #Derick !

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ document.getElementById('home-video').addEventListener('ended', onVideoEnded);
 
 function onVideoEnded (){
   document.getElementById('home-content').style.opacity = 1;
+  var title = document.getElementById('title');
+  if (title.classList){
+    title.classList.add('blink');
+  }else{
+    title.className += ' blink';
+  }
 }
