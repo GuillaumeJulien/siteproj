f02a73173d04ff2c4c5696af8928078bf6e5ea22
Guillaume <Guillaume.Julien@etu.univ-savoie.fr>
2016-03-23 08:55:16 +0100

    Mother fucking animations

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ document.getElementById('home-video').addEventListener('ended', onVideoEnded);
 function onVideoEnded (){
   document.getElementById('home-content').classList.add('home__content--visible');
   document.getElementById('title').classList.add('blink');
+  document.getElementById('credit').classList.add('fadeInDown');  
 }
