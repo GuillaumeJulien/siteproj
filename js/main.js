66c1ee490474ed8d2ce007ce78dba0c4032045e6
Guillaume <Guillaume.Julien@etu.univ-savoie.fr>
2016-03-24 11:39:07 +0100

    Rewind + Disparition du texte + Apparition video

--- js/main.js
+++ js/main.js
@@ -10,4 +10,23 @@ function onVideoEnded (){
  function showTrailer() {
    document.getElementById('title').classList.remove('blink');
    document.getElementById('title').classList.add('blink-exit');
+   rewindVideo();
+   document.getElementsByTagName('body')[0].classList.remove('video-ended');
+   setTimeout(function () {
+
+     video.src = '';
+     video.controls = true;
+   }, 3000);
  }
+function rewindVideo() {
+  var intervalRewind = setInterval(function(){
+    video.playbackRate = 1.0;
+    if(video.currentTime == 0){
+        clearInterval(intervalRewind);
+        video.pause();
+    }
+    else{
+        video.currentTime += -.9;
+    }
+  },150);
+}
